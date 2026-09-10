6252ea8d837c6c4abcda346a78cfc9b708f6fae6
Robert Nelson <robertcnelson@gmail.com>
2026-09-10 14:50:38 -0500

    bump: UBOOT:-v2026.04-Beagle
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -21,7 +21,7 @@ OPTEE_GIT="${OPTEE_GIT:-https://github.com/OP-TEE/optee_os.git}"
 OPTEE="${OPTEE:-4.10.0}"
 
 #https://github.com/beagleboard/u-boot.git
-#https://github.com/beagleboard/u-boot/commits/v2026.01-Beagle/
+#https://github.com/beagleboard/u-boot/commits/v2026.04-Beagle/
 #https://forgejo.gfnd.rcn-ee.org:3000/BeagleBoard.org/u-boot/compare/v2026.01-Beagle...v2026.04-Beagle
 UBOOT_GIT="${UBOOT_GIT:-https://github.com/beagleboard/u-boot.git}"
-UBOOT="${UBOOT:-v2026.01-Beagle}"
+UBOOT="${UBOOT:-v2026.04-Beagle}"
